e3fddc187ecdc7cc5b92914728f5e7581042d087
keycultist <sfmforbobbyaura@gmail.com>
2025-08-18 15:17:25 +0800

    # Class Creatio

---
+++ ConsoleApplication1/Enemy.cpp
@@ -0,0 +1,18 @@
+// Inclusions
+
+#include "Enemy.h"
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <conio.h> 
+#include <windows.h>
+#include <chrono>
+
+// Define namespace
+using namespace std;
+
+// Functions, Classes.
+
--- ConsoleApplication1/Enemy.h
+++ ConsoleApplication1/Enemy.h
@@ -1,1 +1,9 @@
-#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+
+class Enemy
+{
+	
+};
+
+#endif
