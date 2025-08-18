b914093c67042626a68c464f8322e840320c9c09
keycultist <sfmforbobbyaura@gmail.com>
2025-08-18 13:38:55 +0800

    # teresr

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello Worlfdd hi vroddd!\n";
+    std::cout << "Hello Worlfdd hi vdasdasdasroddd!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
