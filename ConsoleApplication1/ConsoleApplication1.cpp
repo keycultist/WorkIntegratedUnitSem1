9741cd03f14be838e6f0810794d6f1c0d231a3d2
worktoast <254123Q@mymail.nyp.edu.sg>
2025-08-18 13:42:11 +0800

    gfv

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello Worlfdd hi vdasdasdasroddd!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Run program: Ctrl + F5 or Deghhgghghbug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
