e45ac270b39b111ab4b5e11fde31a4055c1cade8
keycultist <sfmforbobbyaura@gmail.com>
2025-08-19 12:58:45 +0800

    # Map Begins

---
+++ ConsoleApplication1/Map.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Map
+{
+
+public:
+	
+protected:
+
+private:
+	char Map[128][128];
+
+	char InnerRoom[256][256];
+};
