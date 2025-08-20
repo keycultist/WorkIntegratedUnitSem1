64765039d5499d4cc2209e3a903bab546a9638c2
keycultist <sfmforbobbyaura@gmail.com>
2025-08-20 13:44:29 +0800

    # Movesets for enemies.

--- ConsoleApplication1/Moveset.cpp
+++ ConsoleApplication1/Moveset.cpp
@@ -38,26 +38,39 @@ void Moveset::EnemyInit(const std::string Class)
     //Add enemy movesets
     if (Class == "Cleaver") {
         EnemyNameMoveset.SetMove(Move("Lesser Cleave, Vertical", 1, 4, "Abyssal"));
-		EnemyNameMoveset.SetMove(Move("Lesser Cleave, Horizontal", 6, 1, "Abyssal"));
+		EnemyNameMoveset.SetMove(Move("Lesser Spear", 4, 1, "Abyssal"));
     }
     else if (Class == "Silent") {
         EnemyNameMoveset.SetMove(Move("Alas", 1, 2, "Physical"));
-		EnemyNameMoveset.SetMove(Move("Mook", 1, 6, "Physical"));
+		EnemyNameMoveset.SetMove(Move("Mook", 1, 5, "Physical"));
     }
-    else if (Class == "Asdasda") {
-
+    else if (Class == "Mani") {
+        EnemyNameMoveset.SetMove(Move("Ankle Splitter", 3, 1, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Rising Dragon", 1, 3, "Physical"));
+    }
+    else if (Class == "Grunt") {
+        EnemyNameMoveset.SetMove(Move("Slash", 2, 1, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Dust", 1, 4, "Physical"));
     }
 
     // Special Classes (bosses, etc)
     else if (Class == "ColorCleaver") {
 		EnemyNameMoveset.SetMove(Move("Greater Cleave, Vertical", 2, 4, "Abyssal"));
-		EnemyNameMoveset.SetMove(Move("Greater Cleave, Horizontal", 10, 1, "Abyssal"));
         EnemyNameMoveset.SetMove(Move("Spear", 4, 1, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Upstanding Slash", 3, 1, "Physical"));
+		EnemyNameMoveset.SetMove(Move("Greater Cleave, Horizontal", 25, 1, "Abyssal"));
     }
     else if (Class == "DarkSilence") {
         EnemyNameMoveset.SetMove(Move("Mook Workshop", 2, 6, "Abyssal"));
         EnemyNameMoveset.SetMove(Move("Crystal Atelier", 3, 2, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Furioso", 1, 12, "Abyssal"));
+        EnemyNameMoveset.SetMove(Move("Atelier Logic", 3, 2, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Furioso", 2, 12, "Abyssal"));
+    }
+    else if (Class == "ManiKatti") {
+        EnemyNameMoveset.SetMove(Move("Tatsumaki", 2, 6, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Goshoryu", 3, 4, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Demon Flip", 1, 5, "Physical"));
+        EnemyNameMoveset.SetMove(Move("Shun Goku Satsu", 1, 25, "Physical"));
     }
 }
 
