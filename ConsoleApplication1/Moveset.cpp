8b5c754552be2f4d6173c7df6724c0e72f4fd854
Kabiax <ryanseah088@gmail.com>
2025-08-25 12:48:26 +0800

    How do i

--- ConsoleApplication1/Moveset.cpp
+++ ConsoleApplication1/Moveset.cpp
@@ -33,138 +33,137 @@ int Moveset::size() const {
 
 void Moveset::EnemyInit(const std::string Class)
 {
-    Moveset EnemyNameMoveset; //Found out this is not needed, i think.
 
     //SetMove(Move("MOVE NAME", STRENGTH, HITS, "TYPE"));
 
     //Add enemy movesets
     if (Class == "Cleaver") {
-        EnemyNameMoveset.SetMove(Move("Lesser Cleave, Vertical", 1, 5, "Abyssal"));
-		EnemyNameMoveset.SetMove(Move("Lesser Spear", 4, 1, "Abyssal"));
+        SetMove(Move("Lesser Cleave, Vertical", 1, 5, "Abyssal"));
+		SetMove(Move("Lesser Spear", 4, 1, "Abyssal"));
     }
     else if (Class == "Silent") {
-        EnemyNameMoveset.SetMove(Move("Alas", 1, 2, "Physical"));
-		EnemyNameMoveset.SetMove(Move("Mook", 1, 5, "Physical"));
+        SetMove(Move("Alas", 1, 2, "Physical"));
+		SetMove(Move("Mook", 1, 5, "Physical"));
     }
     else if (Class == "Mani") {
-        EnemyNameMoveset.SetMove(Move("Ankle Splitter", 5, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Rising Dragon", 1, 3, "Physical"));
+        SetMove(Move("Ankle Splitter", 5, 1, "Physical"));
+        SetMove(Move("Rising Dragon", 1, 3, "Physical"));
     }
     else if (Class == "Resonant") {
-        EnemyNameMoveset.SetMove(Move("Trails of Blue", 3, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Tempestuous Danza", 5, 1, "Physical"));
+        SetMove(Move("Trails of Blue", 3, 1, "Physical"));
+        SetMove(Move("Tempestuous Danza", 5, 1, "Physical"));
     }
     else if (Class == "Grunt") {
-        EnemyNameMoveset.SetMove(Move("Slash", 2, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Dust", 1, 4, "Physical"));
+        SetMove(Move("Slash", 2, 1, "Physical"));
+        SetMove(Move("Dust", 1, 4, "Physical"));
     }
     else if (Class == "Necromancer") {
-		EnemyNameMoveset.SetMove(Move("Avaunt", 1, 3, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Lich's Grasp", 5, 1, "Abyssal"));
+		SetMove(Move("Avaunt", 1, 3, "Abyssal"));
+        SetMove(Move("Lich's Grasp", 5, 1, "Abyssal"));
     }
     else if (Class == "Rogue") {
-        EnemyNameMoveset.SetMove(Move("Shadow Step", 3, 1, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Lethality", 1, 4, "Abyssal"));
+        SetMove(Move("Shadow Step", 3, 1, "Abyssal"));
+        SetMove(Move("Lethality", 1, 4, "Abyssal"));
     }
 
     // Special Classes (bosses, etc) Fourth move activates thru conditionals
     else if (Class == "ColorCleaver") {
-		EnemyNameMoveset.SetMove(Move("Greater Cleave, Vertical", 2, 4, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Spear", 4, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Upstanding Slash", 3, 1, "Physical"));
-		EnemyNameMoveset.SetMove(Move("Greater Cleave, Horizontal", 25, 1, "Abyssal"));
+		SetMove(Move("Greater Cleave, Vertical", 2, 4, "Abyssal"));
+        SetMove(Move("Spear", 4, 1, "Physical"));
+        SetMove(Move("Upstanding Slash", 3, 1, "Physical"));
+		SetMove(Move("Greater Cleave, Horizontal", 25, 1, "Abyssal"));
     }
     else if (Class == "DarkSilence") {
-        EnemyNameMoveset.SetMove(Move("Mook Workshop", 2, 6, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Crystal Atelier", 3, 2, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Atelier Logic", 3, 2, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Furioso", 2, 12, "Abyssal"));
+        SetMove(Move("Mook Workshop", 2, 6, "Abyssal"));
+        SetMove(Move("Crystal Atelier", 3, 2, "Abyssal"));
+        SetMove(Move("Atelier Logic", 3, 2, "Physical"));
+        SetMove(Move("Furioso", 2, 12, "Abyssal"));
     }
     else if (Class == "ManiKatti") {
-        EnemyNameMoveset.SetMove(Move("Tatsumaki", 2, 6, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Goshoryu", 3, 4, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Demon Flip", 1, 5, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Shun Goku Satsu", 1, 25, "Abyssal"));
+        SetMove(Move("Tatsumaki", 2, 6, "Physical"));
+        SetMove(Move("Goshoryu", 3, 4, "Physical"));
+        SetMove(Move("Demon Flip", 1, 5, "Physical"));
+        SetMove(Move("Shun Goku Satsu", 1, 25, "Abyssal"));
     }
     else if (Class == "AzureResonance") {
-        EnemyNameMoveset.SetMove(Move("Trails of Blue", 3, 2, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Tempestuous Danza", 5, 2, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Final Impromptu", 6, 2, "Magical"));
-        EnemyNameMoveset.SetMove(Move("Grand Finale", 1, 25, "Abyssal"));
+        SetMove(Move("Trails of Blue", 3, 2, "Physical"));
+        SetMove(Move("Tempestuous Danza", 5, 2, "Physical"));
+        SetMove(Move("Final Impromptu", 6, 2, "Magical"));
+        SetMove(Move("Grand Finale", 1, 25, "Abyssal"));
     }
 
     // True Boss movesets, Phase Change moves will be commented.
     else if (Class == "OneWingedAngel") {
-		EnemyNameMoveset.SetMove(Move("Flare.", 4, 8, "Magical"));
-        EnemyNameMoveset.SetMove(Move("Shadow Flare.", 15, 3, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Octaslash.", 3, 8, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Scintilla.", 0, 1, "Counter"));
+		SetMove(Move("Flare.", 4, 8, "Magical"));
+        SetMove(Move("Shadow Flare.", 15, 3, "Abyssal"));
+        SetMove(Move("Octaslash.", 3, 8, "Physical"));
+        SetMove(Move("Scintilla.", 0, 1, "Counter"));
 
         // Phase Change Only Move, used ONCE per fight.
-		EnemyNameMoveset.SetMove(Move("Supernova.", 1, 150, "BossMove"));
+		SetMove(Move("Supernova.", 1, 150, "BossMove"));
 
 		// if HP <= 1% then use this move.
-        EnemyNameMoveset.SetMove(Move("I refuse to be just a memory.", 1, 225, "DeathMove"));
+        SetMove(Move("I refuse to be just a memory.", 1, 225, "DeathMove"));
     }
     else if (Class == "JovialChaos") {
-        EnemyNameMoveset.SetMove(Move("At the ready.", 5, 1, "Buff"));
-        EnemyNameMoveset.SetMove(Move("Curse.", 0, 1, "Debuff"));
-        EnemyNameMoveset.SetMove(Move("Fire.", 45, 1, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Focus.", 5, 1, "Buff"));
+        SetMove(Move("At the ready.", 5, 1, "Buff"));
+        SetMove(Move("Curse.", 0, 1, "Debuff"));
+        SetMove(Move("Fire.", 45, 1, "Abyssal"));
+        SetMove(Move("Focus.", 5, 1, "Buff"));
 
 		// Phase Change Only Move, used ONCE per fight.
-		EnemyNameMoveset.SetMove(Move("Deus Ex Machina.", 1, 150, "BossMove"));
+		SetMove(Move("Deus Ex Machina.", 1, 150, "BossMove"));
 
 		// if HP <= 1% then use this move.
-        EnemyNameMoveset.SetMove(Move("Door to the Backyard.", 1, 200, "DeathMove"));
+        SetMove(Move("Door to the Backyard.", 1, 200, "DeathMove"));
     }
     else if (Class == "Bob") {
-        EnemyNameMoveset.SetMove(Move("Bob's Biggest Bang", 45, 1, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Bob's Blazar.", 1, 50, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Bob's Bazooka", 45, 1, "Physical"));
-		EnemyNameMoveset.SetMove(Move("Bob's Burgers", 5, 1, "Buff"));
+        SetMove(Move("Bob's Biggest Bang", 45, 1, "Abyssal"));
+        SetMove(Move("Bob's Blazar.", 1, 50, "Abyssal"));
+        SetMove(Move("Bob's Bazooka", 45, 1, "Physical"));
+		SetMove(Move("Bob's Burgers", 5, 1, "Buff"));
 
 		// Phase Change Only Move, used ONCE per fight.
-		EnemyNameMoveset.SetMove(Move("Bob's Black Hole", 1, 150, "BossMove"));
+		SetMove(Move("Bob's Black Hole", 1, 150, "BossMove"));
 
 		// if HP <= 1% then use this move.
-        EnemyNameMoveset.SetMove(Move("Bob's Big Black Broad Bounteous Bountiful Black Breach", 1, 200, "DeathMove"));
+        SetMove(Move("Bob's Big Black Broad Bounteous Bountiful Black Breach", 1, 200, "DeathMove"));
     }
     else if (Class == "DevilGene") {
-		EnemyNameMoveset.SetMove(Move("Electric Wind God Fist.", 45, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Obliteration.", 35, 1, "Physical"));
-        EnemyNameMoveset.SetMove(Move("Devil Gene: Unleashed.", 1, 45, "Physical"));
-		EnemyNameMoveset.SetMove(Move("Linebreaker.", 40, 1, "Physical"));
+		SetMove(Move("Electric Wind God Fist.", 45, 1, "Physical"));
+        SetMove(Move("Obliteration.", 35, 1, "Physical"));
+        SetMove(Move("Devil Gene: Unleashed.", 1, 45, "Physical"));
+		SetMove(Move("Linebreaker.", 40, 1, "Physical"));
 
 		// Phase Change Only Move, used ONCE per fight.
-		EnemyNameMoveset.SetMove(Move("Devil Dimension: Twisted Chains.", 1, 150, "BossMove"));
+		SetMove(Move("Devil Dimension: Twisted Chains.", 1, 150, "BossMove"));
 
 		// if HP <= 1% then use this move.
-		EnemyNameMoveset.SetMove(Move("Demonic Valour.", 1, 200, "DeathMove"));
+		SetMove(Move("Demonic Valour.", 1, 200, "DeathMove"));
     }
     else if (Class == "Susanoo") {
-        EnemyNameMoveset.SetMove(Move("Ouja Zenrouga", 50, 1, "Abyssal"));
-		EnemyNameMoveset.SetMove(Move("Garengeki", 1, 50, "Abyssal"));
-		EnemyNameMoveset.SetMove(Move("Jabaku Fuueinjin", 10, 3, "Abyssal"));
-        EnemyNameMoveset.SetMove(Move("Jarin Renshouga", 20, 2, "Abyssal"));
+        SetMove(Move("Ouja Zenrouga", 50, 1, "Abyssal"));
+		SetMove(Move("Garengeki", 1, 50, "Abyssal"));
+		SetMove(Move("Jabaku Fuueinjin", 10, 3, "Abyssal"));
+        SetMove(Move("Jarin Renshouga", 20, 2, "Abyssal"));
 
 		// Phase Change Only Move, used ONCE per fight.
-		EnemyNameMoveset.SetMove(Move("Senkon Meiraku", 1, 150, "BossMove"));
+		SetMove(Move("Senkon Meiraku", 1, 150, "BossMove"));
 
 		// if HP <= 1% then use this move.
-        EnemyNameMoveset.SetMove(Move("Kamigaeri: Orochizantou Reppuuga", 50, 4, "DeathMove"));
+        SetMove(Move("Kamigaeri: Orochizantou Reppuuga", 50, 4, "DeathMove"));
     }
   //  else if (Class == "TheDemonKing") {
-  //      EnemyNameMoveset.SetMove(Move("BRO WHERE'S THE BALL???", 45, 1, "Physical"));
-  //      EnemyNameMoveset.SetMove(Move("Metavision", 5, 1, "Buff"));
-  //      EnemyNameMoveset.SetMove(Move("Two Gun Volley", 20, 2, "Physical"));
-  //      EnemyNameMoveset.SetMove(Move("FOOTBALL IS NOT THAT SERIOUS??", 1, 45, "Abyssal"));
+  //      SetMove(Move("BRO WHERE'S THE BALL???", 45, 1, "Physical"));
+  //      SetMove(Move("Metavision", 5, 1, "Buff"));
+  //      SetMove(Move("Two Gun Volley", 20, 2, "Physical"));
+  //      SetMove(Move("FOOTBALL IS NOT THAT SERIOUS??", 1, 45, "Abyssal"));
   //      
   //      // Phase Change Only Move, used ONCE per fight.
-  //      EnemyNameMoveset.SetMove(Move("MOVE IT.", 150, 1, "BossMove"));
+  //      SetMove(Move("MOVE IT.", 150, 1, "BossMove"));
 		//
   //      // if HP <= 1% then use this move.
-		//EnemyNameMoveset.SetMove(Move("I want to win,,, WITH MY OWN GOAL!!!!", 200, 1, "DeathMove"));
+		//SetMove(Move("I want to win,,, WITH MY OWN GOAL!!!!", 200, 1, "DeathMove"));
   //  }
 }
 
